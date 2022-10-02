e0c6fed4a4aaadd36804f00de36f81391898b448
Maxim <flipsidebeat@gmail.com>
2022-10-02 10:07:47 +0300

    horoscope 0.1

---
+++ horoscope/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views  # . - это импорт из той же директории
+
+
+# здесь создается конфигурация URL
+urlpatterns = [
+    path('', views.index),
+    path('<int:sign_zodiac>', views.get_info_about_sign_zodiac_by_number),
+    path('<str:sign_zodiac>', views.get_info_about_sign_zodiac, name='horoscope_name')
+]
