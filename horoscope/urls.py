57fb094ebf9f3790fcb106df1bd582e3b713e0fe
Maxim <flipsidebeat@gmail.com>
2022-10-02 10:11:48 +0300

    horoscope 0.1

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index),
     path('<int:sign_zodiac>', views.get_info_about_sign_zodiac_by_number),
     path('<str:sign_zodiac>', views.get_info_about_sign_zodiac, name='horoscope_name')
+    path('type', )
 ]
