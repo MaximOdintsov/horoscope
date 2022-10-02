913b3330c81a2c778ec3b4f501ccf4fe476cc6f0
Maxim <flipsidebeat@gmail.com>
2022-10-02 15:54:17 +0300

    horoscope 0.3

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -5,6 +5,7 @@ from . import views  # . - это импорт из той же директор
 # здесь создается конфигурация URL
 urlpatterns = [
     path('', views.index),
+    path('<int:month>/<int:day>', views.get_info_by_date),
     path('<str:types>/', views.types_of_zodiac_sign, name='types_name'),
     path('<str:types>/<str:types_sign_zodiac>', views.get_info_about_the_type_of_zodiac_sign, name='types_sign_name'),
     path('<int:sign_zodiac>', views.get_info_about_sign_zodiac_by_number),
--- horoscope/views.py
+++ horoscope/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http.response import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
+import datetime
+
 # Create your views here.
 zodiac_dict = {
     'aries': "Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).",
@@ -16,6 +18,21 @@ zodiac_dict = {
     'capricorn': "Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).",
     'aquarius': "Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).",
     'pisces': "Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта)."
+}
+
+dates_dict = {
+    'aries': ['3/21', '4/20'],
+    'taurus': ['4/21', '5/21'],
+    'gemini': ['5/22', '6/21'],
+    'cancer': ['6/22', '7/22'],
+    'leo': ['7/23', '8/21'],
+    'virgo': ['8/22', '9/23'],
+    'libra': ['9/24', '10/23'],
+    'scorpio': ['10/24', '11/22'],
+    'sagittarius': ['11/23', '12/22'],
+    'capricorn': ['12/23', '1/20'],
+    'aquarius': ['1/21', '2/19'],
+    'pisces': ['2/20', '3/20'],
 }
 
 types_list = ['fire', 'earth', 'air', 'water']
@@ -50,7 +67,6 @@ def get_info_about_the_type_of_zodiac_sign(request, types, types_sign_zodiac):
     for sign in type_elements:
         redirect_path = reverse("horoscope_name", args=[sign])
         description_elements += f'<li> <a href="{redirect_path}"> {sign.title()} </a> </li>'
-
     response = f'''
     <ul>
         {description_elements}
@@ -65,10 +81,16 @@ def index(request):
     for sign in zodiacs:
         redirect_path = reverse("horoscope_name", args=[sign])
         li_elements += f'<li> <a href="{redirect_path}"> {sign.title()} </a> </li>'
+
+    redirect_path_for_types = reverse("types_name", args=['types'])
+    types_zodiac = f'<h4> <a href="{redirect_path_for_types}"> Types of zodiac signs </a> </h4>'
+
     response = f'''
+    <br> 
     <ol>
+        {types_zodiac}
         {li_elements}
-    </ol>    
+    </ol>
     '''
 
     return HttpResponse(response)
@@ -91,3 +113,38 @@ def get_info_about_sign_zodiac_by_number(request, sign_zodiac: int):
     name_zodiac = zodiacs[sign_zodiac-1]
     redirect_url = reverse("horoscope_name", args=[name_zodiac])
     return HttpResponseRedirect(redirect_url)
+
+
+def date_input(date1, date2, date3):  # * - обязательный аргумент
+
+    sign = None
+    dt1 = datetime.datetime.strptime(date1, "%m/%d")
+    dt2 = datetime.datetime.strptime(date2, "%m/%d")
+    dt3 = datetime.datetime.strptime(date3, "%m/%d")
+    if dt1 <= dt3 <= dt2:
+        sign = True
+    else:
+        sign = False
+    return sign
+
+
+def get_info_by_date(request, month, day):
+    if (1 <= month <= 12) and (1 <= day <= 31):
+        for key in dates_dict:
+            # print(key)
+            description = dates_dict.get(key)
+
+            date1 = description[0]
+            date2 = description[1]
+
+            sign = date_input(date1, date2, f'{month}/{day}')
+
+            if sign == True:
+                redirect_url = reverse("horoscope_name", args=[key])
+                return HttpResponseRedirect(redirect_url)
+
+        redirect_url = reverse("horoscope_name", args=['capricorn'])
+        return HttpResponseRedirect(redirect_url)
+    else:
+        return HttpResponseNotFound('Неправильно введена дата!')
+# print(date_init('3/10'))
